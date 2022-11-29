c897e4e067c0cd71fe4bb55e39d412ea63fb7d96
Dashch <dashch88@gmail.com>
2022-11-29 19:07:01 +0400

    Added Task 3. Finding difference btwn max and min elements of array.

---
+++ CSharp_HW5_Task3_DiffMaxMin/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+int[] array = new int[5];
+int minNum = 99;
+int maxNum = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write($"{array[i]} ");
+    if (array[i] > maxNum) maxNum = array[i];
+    if (array[i] < minNum) minNum = array[i];
+}
+Console.WriteLine();
+
+int diffMaxMin = maxNum - minNum;
+
+Console.WriteLine($"Максимальный элемент массива: {maxNum}");
+Console.WriteLine($"Минимальный элемент массива: {minNum}");
+Console.WriteLine($"Разница между max и min элементами: {diffMaxMin}");
